1d7e036d5438673b42185317eaa1811232df6aa5
TZGyn <tzgyn@TZGyns-MacBook-Pro.local>
2026-08-01 20:28:55 +0800

    rebuild v0.0.1 DMG

--- Casks/koins.rb
+++ Casks/koins.rb
@@ -1,6 +1,6 @@
 cask "koins" do
   version "0.0.1"
-  sha256 "6ce0399ab701af8e254b864938ef7072e42b8d458ec48909bbec471ada3070e6"
+  sha256 "3e435477d5a93e405e35423b8598793920f231985d25803ead957ff3a9910156"
 
   url "https://github.com/TZGyn/koins/releases/download/v#{version}/stable-macos-arm64-koins.dmg"
   name "Koins"
